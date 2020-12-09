877f49bfd266e798756ad9c09bcf3dea033552c5
yanh1993 <891367701@qq.com>
2020-12-09 23:39:30 +0800

    'up'

---
+++ excelTOsql/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace excelTOsql
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.filenamebox = new System.Windows.Forms.TextBox();
+            this.excelBookComboBoxEx = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(322, 37);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 33);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "浏览...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // filenamebox
+            // 
+            this.filenamebox.Location = new System.Drawing.Point(91, 41);
+            this.filenamebox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.filenamebox.Name = "filenamebox";
+            this.filenamebox.Size = new System.Drawing.Size(198, 23);
+            this.filenamebox.TabIndex = 1;
+            // 
+            // excelBookComboBoxEx
+            // 
+            this.excelBookComboBoxEx.FormattingEnabled = true;
+            this.excelBookComboBoxEx.Location = new System.Drawing.Point(91, 101);
+            this.excelBookComboBoxEx.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.excelBookComboBoxEx.Name = "excelBookComboBoxEx";
+            this.excelBookComboBoxEx.Size = new System.Drawing.Size(116, 25);
+            this.excelBookComboBoxEx.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(322, 93);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(87, 33);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "导入";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "文件目录：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "表单名称：";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(415, 163);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.excelBookComboBoxEx);
+            this.Controls.Add(this.filenamebox);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "EXCEL to SQL";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox filenamebox;
+        private System.Windows.Forms.ComboBox excelBookComboBoxEx;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ excelTOsql/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+using System.Data.OleDb;
+namespace excelTOsql
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        OleDbConnection oledbcConnection;
+        string connString="server=localhost;uid=sa;pwd=123;database=TTT";//这里是自己的数据库信息，根据自己的情况修改
+        /*打开excel文件并选择表单*/
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog openDG = new OpenFileDialog();
+                openDG.Title = "打开Excel表格";
+                //openDG.Filter = "Excel表格(*.xlsx)|*.xlsx|CSV格式(*.csv)|*.csv|所有文件(*.*)|*.*";
+                openDG.ShowDialog();
+                string filename;//文件路径
+                filename = openDG.FileName;
+                filenamebox.Text = filename;//显示文件路径
+                string strConn = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + filename + ";Extended Properties='Excel 12.0;HDR=False;IMEX=1'";//此处为对excel的读取设置，不同的excel版本有不同的设定。
+                
+                
+                //@"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + filename + ";Extended Properties=Excel 8.0";
+               /*读取excel数据到内存中*/
+                oledbcConnection = new OleDbConnection(strConn);
+                oledbcConnection.Open();
+                DataTable table = new DataTable();
+                table = oledbcConnection.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, new object[] { null, null, null, "TABLE" });
+                /*获取此文件中的所有表单*/
+                excelBookComboBoxEx.Items.Clear();
+                foreach (DataRow dr in table.Rows)
+                {
+                    excelBookComboBoxEx.Items.Add((String)dr["TABLE_NAME"]);
+                }
+                excelBookComboBoxEx.Text = excelBookComboBoxEx.Items[0].ToString();
+
+                DataSet ds = new DataSet();
+                SqlConnection conn = new SqlConnection(connString);
+
+                string strConn1 = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + filenamebox.Text + ";Extended Properties='Excel 12.0;HDR=False;IMEX=1'";// "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + filePath + ";Extended Properties='Excel 12.0;HDR=False;IMEX=1'";
+                //"Provider=Microsoft.Jet.OLEDB.4.0;" + "Data Source=" + excelFile + ";" + "Extended Properties=Excel 8.0;";
+                OleDbConnection comm = new OleDbConnection(strConn1);
+                comm.Open();
+
+                string strExcel = "";
+                OleDbDataAdapter myCommand = null;
+                strExcel = string.Format("select * from [{0}]", excelBookComboBoxEx.Text);
+                myCommand = new OleDbDataAdapter(strExcel, strConn1);
+                myCommand.Fill(ds, excelBookComboBoxEx.Text);
+                //filenamebox.Text = ds.Tables[0].Rows.Count.ToString();/*显示导入数据的总条数*/
+                comm.Close();
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+        }
+
+        public void TransferData(string excelFile, string sheetName, string connectionString)
+        {
+            DataSet ds = new DataSet();
+            try
+            {
+                //获取全部数据
+                string strConn = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + excelFile + ";Extended Properties='Excel 12.0;HDR=False;IMEX=1'";// "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + filePath + ";Extended Properties='Excel 12.0;HDR=False;IMEX=1'";
+                //"Provider=Microsoft.Jet.OLEDB.4.0;" + "Data Source=" + excelFile + ";" + "Extended Properties=Excel 8.0;";
+                OleDbConnection conn = new OleDbConnection(strConn);
+                conn.Open();
+                string strExcel = "";
+                OleDbDataAdapter myCommand = null;
+                strExcel = string.Format("select * from [{0}]", sheetName);
+                myCommand = new OleDbDataAdapter(strExcel, strConn);
+                myCommand.Fill(ds, sheetName);
+                //如果目标表不存在则创建
+                string strSql = string.Format("if object_id('{0}') is null create table {0}(", sheetName);
+                foreach (System.Data.DataColumn c in ds.Tables[0].Columns)
+                {
+                    strSql += string.Format("[{0}] varchar(255),", c.ColumnName);
+                }
+                strSql = strSql.Trim(',') + ")";
+                using (System.Data.SqlClient.SqlConnection sqlconn = new System.Data.SqlClient.SqlConnection(connectionString))
+                {
+                    sqlconn.Open();
+                    System.Data.SqlClient.SqlCommand command = sqlconn.CreateCommand();
+                    command.CommandText = strSql;
+                    command.ExecuteNonQuery();
+                    sqlconn.Close();
+                }
+                //用bcp导入数据
+                using (System.Data.SqlClient.SqlBulkCopy bcp = new System.Data.SqlClient.SqlBulkCopy(connectionString))
+                {
+                    bcp.BatchSize = 100;//每次传输的行数
+                    bcp.NotifyAfter = 100;//进度提示的行数
+                    bcp.DestinationTableName = sheetName;//目标表
+                    bcp.WriteToServer(ds.Tables[0]);
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string FILE_NAME = filenamebox.Text;      
+            string ST = excelBookComboBoxEx.Text;
+            TransferData(FILE_NAME, ST, connString);
+            MessageBox.Show("导入成功!");
+        }     
+    }
+}
